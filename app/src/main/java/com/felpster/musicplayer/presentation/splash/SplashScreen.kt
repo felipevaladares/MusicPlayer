52868a6b7b2bda7c3206a5a763276d7e54d96522
Felipe Valadares <felipe.valadares2@gmail.com>
2026-05-02 18:34:51 -0300

    Add custom basic splash screen

---
+++ app/src/main/java/com/felpster/musicplayer/presentation/splash/SplashScreen.kt
@@ -0,0 +1,48 @@
+package com.felpster.musicplayer.presentation.splash
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.felpster.core_ui.R
+import com.felpster.core_ui.theme.MusicPlayerTheme
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_default_background),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize() // Matches the parent Box size
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_musical_note),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.size(100.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    MusicPlayerTheme {
+        SplashScreen()
+    }
+}
