1a6bc5553192dab80aa19a9c53671ee472a78500
Felipe Valadares <felipe.valadares2@gmail.com>
2026-05-02 16:52:39 -0300

    Centralize UI dependencies on core-ui using api declaration, remove duplicates from app build.gradle

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -30,18 +30,13 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.material)
+    api(platform(libs.androidx.compose.bom))
+    api(libs.androidx.activity.compose)
+    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.ui)
+    api(libs.androidx.compose.ui.graphics)
+    api(libs.androidx.compose.ui.tooling.preview)
+    api(libs.material)
 
     testImplementation(libs.junit)
 
