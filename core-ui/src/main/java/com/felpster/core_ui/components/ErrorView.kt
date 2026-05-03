3a774c77d1227ee071793b9a7b6c803373894e1e
Felipe Valadares <felipe.valadares2@gmail.com>
2026-05-02 21:03:49 -0300

    Add basic error view

---
+++ core-ui/src/main/java/com/felpster/core_ui/components/ErrorView.kt
@@ -0,0 +1,56 @@
+package com.felpster.core_ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.felpster.core_ui.R
+import com.felpster.core_ui.components.ErrorLayoutTags.ERROR_LAYOUT
+import com.felpster.core_ui.theme.MusicPlayerTheme
+
+@Composable
+fun ErrorView(
+    message: String?,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.testTag(ERROR_LAYOUT),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_sad_face),
+            contentDescription = null,
+        )
+
+        Text(
+            modifier = Modifier.padding(top = 8.dp),
+            style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Black),
+            text = message ?: stringResource(id = R.string.generic_error_message),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorViewPreview() {
+    MusicPlayerTheme {
+        ErrorView(message = null)
+    }
+}
+
+object ErrorLayoutTags {
+    const val ERROR_LAYOUT = "ErrorLayoutTags_content"
+}
