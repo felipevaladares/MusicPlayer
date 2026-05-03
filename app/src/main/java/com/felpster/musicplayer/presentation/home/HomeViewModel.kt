2cc5e8feaf345da63ebe4b16940ec28e68b523bd
Felipe Valadares <felipe.valadares2@gmail.com>
2026-05-03 00:30:37 -0300

    - mock list of songs

--- app/src/main/java/com/felpster/musicplayer/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/felpster/musicplayer/presentation/home/HomeViewModel.kt
@@ -4,13 +4,47 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.felpster.musicplayer.domain.model.Song
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+
+val mockSongs = listOf(
+    Song(
+        id = "1",
+        title = "Purple Rain",
+        artist = "Prince",
+        albumArtUrl = "https://via.placeholder.com/56"
+    ),
+    Song(
+        id = "2",
+        title = "Power Of Equality",
+        artist = "Red Hot Chili Peppers",
+        albumArtUrl = "https://via.placeholder.com/56"
+    ),
+    Song(
+        id = "3",
+        title = "Something",
+        artist = "The Beatles",
+        albumArtUrl = "https://via.placeholder.com/56"
+    ),
+    Song(
+        id = "4",
+        title = "Like A Virgin",
+        artist = "Madonna",
+        albumArtUrl = "https://via.placeholder.com/56"
+    ),
+    Song(
+        id = "5",
+        title = "Get Lucky",
+        artist = "Daft Punk feat. Pharrell Williams",
+        albumArtUrl = "https://via.placeholder.com/56"
+    )
+)
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
 
 ): ViewModel() {
-    var homeViewState by mutableStateOf(HomeViewState.Loading("Loading..."))
+    var homeViewState by mutableStateOf(HomeViewState.Success(mockSongs))
         private set
 }
